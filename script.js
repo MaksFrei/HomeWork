28121c207fe376a9d760476094a0bacd6bf01261
Vladislav <maksim.frei@gmail.com>
2019-04-22 10:50:55 +0300

    Second commit

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 "use strict";
 
-let expNames = [];
-let expCosts = [];
 let money = prompt("Ваш бюджет на месяц?", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
-
-expNames[0] = prompt("Введите обязательную статью расходов в этом месяце");
-expCosts[0] = prompt("Во сколько это обойдется?");
-expNames[1] = prompt("Введите обязательную статью расходов в этом месяце");
-expCosts[1] = prompt("Во сколько это обойдется?");
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {
-        [expNames[0]]: expCosts[0],
-        [expNames[1]]: expCosts[1]
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 };
 
-alert(+money/30);
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце");
+    let b = prompt("Во сколько это обойдется?");
+    appData.expenses[a] = b;
+}
+
+alert(appData.budget / 30);
 console.log(appData);
